cd7c99dd4087eff1f69f83603ee55b5445a093fc
Alvaro Naveda <alvaro.naveda@ticketbis.com>
2016-07-04 20:50:24 +0200

    Custom deploy commit message

--- config.rb
+++ config.rb
@@ -30,7 +30,7 @@ activate :deploy do |deploy|
   # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
   # deploy.branch   = 'custom-branch' # default: gh-pages
   # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
-  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+  deploy.commit_message = 'Static content deploy'
 end
 
 ###

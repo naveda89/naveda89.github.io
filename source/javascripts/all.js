52dd9822a075826a34a5fb7b089b21e189539561
Alvaro Naveda <alvaro.naveda@ticketbis.com>
2016-09-10 02:32:24 +0200

    Some UI changes. Added LinkedIn link

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,6 @@
 // This is where it all goes :)
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', '/javascripts/vendor/particlesjs-config.json', function() {
+    //console.log('callback - particles.js config loaded');
+});
